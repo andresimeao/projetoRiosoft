6413b241a5ade0c5a3e07a0197d5fb9cad5e0235
Andre Luis <andreluis2608@gmail.com>
2021-07-29 12:43:29 -0300

    formulário de login

--- login.php
+++ login.php
@@ -1,12 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
-</head>
-<body>
+<?php require 'header.php'?>
+
+<div class="container">
+    <h1 style="text-align: center;">Login</h1>
+
+<form action="index.php" method="POST">
+  <div class="mb-3">
+    <label for="email1" class="form-label">Email:</label>
+    <input type="email" name="email" class="form-control" id="email" aria-describedby="email" placeholder="Digite seu e-mail" autofocus>
+  </div>
+
+  <div class="mb-3">
+    <label for="senha1" class="form-label">Senha</label>
+    <input type="password" name="senha" class="form-control" id="senha" placeholder="Digite sua senha">
+  </div>
+
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="lembre">
+    <label class="form-check-label" name="lembrar" for="lembre1">Lembre de mim</label>
+
+  </div>
+ 
+  <button type="submit" name="logar" class="btn btn-primary">Login</button> <br>
+  <a href="cadastrar.php">Não possui cadastro clique aqui !</a>
+</form>
+</div>
+
+
+<?php require 'footer.php'?>
     
-</body>
-</html>
